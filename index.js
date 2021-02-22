bf3e76374cc11091cf78dcbd2032c1f1915dcbe8
Wasiu Idowu <hoxtygen@live.com>
2021-02-22 11:56:54 +0100

    feat: Create endpoint for email
    Created email endpoint for sending mails to my designated email. This
    api will be attached to the frontend application, and will be made used of
    by whomever wish to contact me via the portfolio website.

---
+++ index.js
@@ -0,0 +1,63 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const nodemailer = require("nodemailer");
+const cors = require("cors")
+const app = express();
+
+const PORT = process.env.PORT || 9000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/api/form", (req, res) => {
+  nodemailer.createTestAccount((err, account) => {
+    const htmlEmail = `
+<h3>Contact Details</h3>
+<ul>
+<li>Name: ${req.body.name}</li>
+<li>Email: ${req.body.email}</li>
+</ul>
+<h3>Message</h3>
+<p>${req.body.message}</p>
+
+      `;
+
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      auth: {
+        user: process.env.SERVICEACCESS,
+        pass: process.env.SERVICEACCESSNETWORK,
+      },
+    });
+
+    let mailOptions = {
+      from: process.env.SERVICEACCESS,
+      to: process.env.SERVICEACCESSNETWORKRECEIVER,
+      replyTo: process.env.SERVICEACCESSNETWORKRECEIVER,
+      subject: "New Message from Portfolio site",
+      text: req.body.message,
+      html: htmlEmail,
+    };
+
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        return res.status(400).json({
+          status: 400,
+          message: "Error Sending message",
+        });
+      }
+     return res.status(200).json({
+        status: 200,
+        message: "Message sent successfully",
+      });
+    });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
